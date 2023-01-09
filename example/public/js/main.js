761abc4fe25f1f6d9ebe6309065680e5323c8170
pozasl <loic.pozas@wyca.fr>
2023-01-09 09:55:50 +0100

    [MOD] Updated sample

--- example/public/js/main.js
+++ example/public/js/main.js
@@ -4,35 +4,88 @@ import { TourSvc } from '../node_modules/@wyca-robotics/wyca-tour-svc/src/TourSv
 
 // default options with a path to MapData
 const options = {
-    criticalFailure: false, // Should simulated failure be critical
-    failOnInit: false, // Simulate a failure on Init
-    failOnDock: false, // Simulate a failure on its way to the docking station
-    failOnGoToCharge: false, // Simulate a failure when asking to go to dock
-    failOnPoiId: -1, // Simulate a failure on its way to a POI'sid (-1 for none)
-    failOnGotoPoiId: -1, // Simulate a failure when asking to go to a POI'sid (-1 for none)
-    mapDataPath: "mock/map/map_data.json", // MapData json file to simulate the robot's current MapData
-    etaRange: {min: 50, max: 75} // The ranged duration of goto (POI & Charge) actions'simulation.
+  criticalFailure: false, // Should simulated failure be critical
+  failOnInit: false, // Simulate a failure on Init
+  failOnDock: false, // Simulate a failure on its way to the docking station
+  failOnGoToCharge: false, // Simulate a failure when asking to go to dock
+  failOnPoiId: -1, // Simulate a failure on its way to a POI'sid (-1 for none)
+  failOnGotoPoiId: -1, // Simulate a failure when asking to go to a POI'sid (-1 for none)
+  mapDataPath: "mock/map/map_data.json", // MapData json file to simulate the robot's current MapData
+  etaRange: { min: 50, max: 75 } // The ranged duration of goto (POI & Charge) actions'simulation.
 }
+
+const mc = new MockingClient(options)
+const svc = new TourSvc(mc)
 
 document.addEventListener('DOMContentLoaded', onDocumentLoaded)
 
+// Tour initialization on document laoded.
 function onDocumentLoaded()
 {
-    const mc = new MockingClient(options)
-    const svc = new TourSvc(mc)
+  // Initialize the TourSvc with a list of TourPoi wich returns a Promise once the service is initialized
+  let pois = []
+  pois.push(new TourPoi(12, "POI 1", "img/poi_1.png", "video/poi_1.mp4"))
+  pois.push(new TourPoi(13, "POI 2", "img/poi_2.png", "video/poi_2.mp4"))
+  pois.push(new TourPoi(14, "POI 3", "img/poi_3.png", "video/poi_3.mp4"))
+  // Of course ToutPoi id must match the MapData's pois id_poi otherwise it will be rejected
 
-    // Initialize the TourSvc with a list of TourPoi wich returns a Promise once the service is initialized
-    let pois = []
-    pois.push(new TourPoi(12, "POI 1", "img/poi_1.png", "video/poi_1.mp4"))
-    pois.push(new TourPoi(13, "POI 2", "img/poi_2.png", "video/poi_2.mp4"))
-    pois.push(new TourPoi(14, "POI 3", "img/poi_3.png", "video/poi_3.mp4"))
-    // Of course ToutPoi id must match the MapData's pois id_poi otherwise it will be rejected
-
-    svc.init(pois)
-    .then(()=> {
-        console.log("All's set, ready to go !")
+  svc.init(pois)
+    .then(() =>
+    {
+      console.log("All's set, ready to go !")
     })
-    .catch((failure) => {
-        console.log("Uho ! Something went wrong.", failure)
+    .catch((failure) =>
+    {
+      console.log("Uho ! Something went wrong.", failure)
     })
 }
+
+// Go to next POI
+function onNextBtnClick()
+{
+  svc.next()
+    .then((poi) =>
+    {
+      console.log("Reached the POI, now i can play its video", poi.videoPath)
+    })
+    .catch((failure) =>
+    {
+      if (failure.critical)
+      {
+        console.log("Oh no the Robot's got a problem, call a technician !")
+      }
+      else
+      {
+        console.log("Something's blocking the way, try again (Resume) ? Or skip to next POI (Next) ? or cancel the tour (Cancel) ?")
+      }
+    })
+}
+
+// Resume the tour on None critical error
+function onResumeBtnClick()
+{
+  svc.resume()
+    .then((poi) =>
+    {
+      console.log("Reached the POI, now i can play its video", poi.videoPath)
+    })
+    .catch((failure) =>
+    {
+      console.log("Oh no ! Not again :(")
+    })
+}
+
+// Cancel the tour and send the AMR back to its docking station
+function onCancelBtnClick()
+{
+  svc.cancel()
+    .then(() =>
+    {
+      console.log("AMR is back on its docking")
+    })
+    .catch((failure) =>
+    {
+      console.log("AMR couldn't reach it docking station")
+    })
+}
+
